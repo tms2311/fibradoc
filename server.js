597b33dd2ed729d06314c1fa06c7d10df01b4127
tms2311 <tms2311@gmail.com>
2026-03-03 17:14:32 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { Pool } = require("pg");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: 5432
+});
+
+app.get("/", (req, res) => {
+  res.send("FibraDoc rodando 🚀");
+});
+
+app.get("/caixas", async (req, res) => {
+  const result = await pool.query("SELECT * FROM caixas");
+  res.json(result.rows);
+});
+
+app.post("/caixas", async (req, res) => {
+  const { nome, latitude, longitude } = req.body;
+  await pool.query(
+    "INSERT INTO caixas (nome, latitude, longitude) VALUES ($1,$2,$3)",
+    [nome, latitude, longitude]
+  );
+  res.send("Caixa cadastrada");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
